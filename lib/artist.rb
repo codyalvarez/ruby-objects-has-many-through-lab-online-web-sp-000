0ffa6d5451968bdf6a2e83531f5e842f85fbf9f7
Cody Alvarez <cody_alvarez19@outlook.com>
2021-04-06 00:47:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,23 @@
 class Artist
   attr_accessor :name
 
-  @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
   def songs
-    Song.all.select { |song| song.artist == self }
+    @songs
   end
 
   def genres
-    songs.map { |song| song.genre }
-  end
-
-  def self.all
-    @@all
-  end
-
-
-  def new_song(song, genre)
-    Song.new(name, self, genre)
+    @songs.collect do |song|
+      song.genre
+    end
   end
 end
